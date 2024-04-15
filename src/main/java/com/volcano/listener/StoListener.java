a6d144b82dd9fb4754eeea61ba263a014119ffc5
Alex <46887064+alex-oos@users.noreply.github.com>
2024-04-15 19:39:28 +0800

    修改公司名称

--- src/main/java/com/volcano/listener/StoListener.java
+++ src/main/java/com/volcano/listener/StoListener.java
@@ -12,7 +12,7 @@ public class StoListener {
 
     @Resource
     private STOService stoService;
-
+    // 为了更快使用监听器，后期也可以不用，监听器需要增加一个发布者，发布者，发布一个内容，然后监听器监听到内容，开始发送消息，多用于短信服务，或消息分发
     @EventListener
     @Async
     public void onApplicationEvent(StoEvent event) {
